6bd38a06eaaae7cdb84eb0d5d7cc636f5d733a4b
Justin Devasahayam <138565559+JustinDScripts@users.noreply.github.com>
2024-08-02 10:56:37 +0530

    2 test files are created

---
+++ Test.ps1
@@ -0,0 +1,6 @@
+# JD this is the first .ps1 file created from vsCode to the Github Repository.
+# Let's check how it goes
+# Do I wanna create seperate folders for each script etc.
+Get-Date
+Get-ChildItem $Env:USERPROFILE
+$Env:USERPROFILE
---
+++ Test2.ps1
@@ -0,0 +1,4 @@
+# This is 2nd ps1 file from VS code to Githum
+# To see where this file is created, do we want to create sub folders for each script etc
+Get-Date
+Get-ChildItem
